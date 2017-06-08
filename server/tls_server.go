808ef473129500cbdb8b1959ee6e88ea526916ac
jay <blossome223@gmail.com>
2017-06-08 10:45:47 +0900

    merging branch

--- server/tls_server.go
+++ server/tls_server.go
@@ -27,56 +27,6 @@ func (tlsServer *TlsServer) Accept() {
 
 }
 
-func (tlsServer *TlsServer) Init() {
-	servermodule.RegisterAPI((*TlsServer).Receive, APIkeys.Receive)
-	servermodule.RegisterAPI((*TlsServer).Accept, APIkeys.Accept)
-	cer, err := tls.LoadX509KeyPair("./src/murgo/config/server.crt", "./src/murgo/config/server.key")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	//server start to listen on tls
-	tlsConfig := &tls.Config{Certificates: []tls.Certificate{cer}}
-	tlsServer.ln, err = tls.Listen(config.CONN_TYPE, config.DEFAULT_PORT, tlsConfig)
-
-	/*ln, err := net.Listen(config.CONN_TYPE, config.DEFAULT_PORT)
-	tlsServer.ln = ln*/
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	//todo : accept routine into framework
-	// todo : make accept as a cast req
-
-	servermodule.Cast(APIkeys.Accept)
-}
-
-/*
-func (ts *TlsServer) HandleCast(request string, args ...interface{}) {
-
-	if args == nil {
-		reflect.ValueOf(ts).MethodByName(request).Call([]reflect.Value{})
-	} else {
-		inputs := make([]reflect.Value, len(args))
-		for i, _ := range args {
-			inputs[i] = reflect.ValueOf(args[i])
-		}
-		reflect.ValueOf(ts).MethodByName(request).Call(inputs)
-	}
-}
-
-func (ts *TlsServer) HandleCall(request string, args ...interface{}) {
-	if args == nil {
-		reflect.ValueOf(ts).MethodByName(request).Call([]reflect.Value{})
-	} else {
-		inputs := make([]reflect.Value, len(args))
-		for i, _ := range args {
-			inputs[i] = reflect.ValueOf(args[i])
-		}
-		reflect.ValueOf(ts).MethodByName(request).Call(inputs)
-	}
-}*/
-
 func (ts *TlsServer) terminate() {
 	ts.ln.Close()
 	//todo : 메모리 회수 및 나머지 작업
@@ -115,4 +65,29 @@ func (t *TlsServer) Receive(client *Client) {
 		}
 		servermodule.Cast(APIkeys.HandleMessage, msg)
 		servermodule.Cast(APIkeys.Receive, client)*/
+}
+
+//callback
+func (tlsServer *TlsServer) Init() {
+	servermodule.RegisterAPI((*TlsServer).Receive, APIkeys.Receive)
+	servermodule.RegisterAPI((*TlsServer).Accept, APIkeys.Accept)
+	cer, err := tls.LoadX509KeyPair("./src/murgo/config/server.crt", "./src/murgo/config/server.key")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//server start to listen on tls
+	tlsConfig := &tls.Config{Certificates: []tls.Certificate{cer}}
+	tlsServer.ln, err = tls.Listen(config.CONN_TYPE, config.DEFAULT_PORT, tlsConfig)
+
+	/*ln, err := net.Listen(config.CONN_TYPE, config.DEFAULT_PORT)
+	tlsServer.ln = ln*/
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//todo : accept routine into framework
+	// todo : make accept as a cast req
+
+	servermodule.Cast(APIkeys.Accept)
 }
